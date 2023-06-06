9a01339a41fe7ba394244fb18c17371b8a069722
richer <persona1241@naver.com>
2023-06-06 11:27:28 +0900

    닉네임변경확인 > 입력 후 완료 클릭시 데이터 들고 오기

--- app/src/main/java/com/example/intentpractice/EditNickNameActivity.kt
+++ app/src/main/java/com/example/intentpractice/EditNickNameActivity.kt
@@ -1,5 +1,6 @@
 package com.example.intentpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.intentpractice.databinding.ActivityEditNickNameBinding
@@ -10,5 +11,18 @@ class EditNickNameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditNickNameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.okBtn.setOnClickListener {
+
+            //닉네임 입력창 정보 가져오기
+            val inputNewNickname = binding.newNicknameEdt.text.toString()
+
+            //입력닉네임을 담아주기 위한 용도로 인텐트를 만든다
+            val resultIntent = Intent()
+            resultIntent.putExtra("nickname",inputNewNickname)
+            setResult(RESULT_OK,resultIntent)
+            finish()
+
+        }
     }
 }
